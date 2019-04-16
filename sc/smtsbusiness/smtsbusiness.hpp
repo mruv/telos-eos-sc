ab380ae7074ab00e7d9e6362e485212ed0293c1e
Vincent Mwendwa <vrubben@outlook.com>
2019-04-16 10:39:16 +0300

    changed Table name cunitprice to cunitprices

--- sc/smtsbusiness/smtsbusiness.hpp
+++ sc/smtsbusiness/smtsbusiness.hpp
@@ -20,15 +20,11 @@ using eosio::symbol_code;
 
 namespace SmtsBusiness {
 
-	// Inventory Request Status
-	// enum class IrStatus : uint8_t { PENDING = 0, FULFILLED = 1, PAID = 2 };
-
 	class [[eosio::contract("smtsbusiness")]] SmtsBusiness : public contract {
 
 		public:
 			SmtsBusiness(name self, name code, datastream<const char*> ds) : contract(self, code, ds) {}
 
-			// YELOS asset creation and distribution
 			[[eosio::action("createcurr")]]   void CreateToken(const asset& max_supply);
 			[[eosio::action("createcomm")]]   void CreateCmmdty(const name& issuer, const asset& max_supply, const asset& unit_price);
 			[[eosio::action("issue")]]        void Issue(const name& to, const asset& quantity, const std::string& memo);
@@ -37,28 +33,20 @@ namespace SmtsBusiness {
 			[[eosio::action("fulfill")]]      void Fulfill(const name& merchant_acct, uint64_t ir_id);
 			[[eosio::action("pay")]]          void Pay(const name& customer_acct, uint64_t ir_id);
 
-
 			[[eosio::action("deletedata")]]   void DeleteData();
+
+		private:
 
 			void Create(const name& issuer, const asset& max_supply);
 			void UpdateDestAcct(const name& from, const name& to, const asset& value);
 			void UpdateSrcAcct(const name& owner, const asset& value);
-
-			// INVENTORY
-
-		private:
 			
-			// A data structure that defines the amount / quantity of an asset held by
-			// a single account.
 			struct [[eosio::table("account")]] Account {
 				asset    balance;
 
 				uint64_t primary_key() const { return balance.symbol.raw(); }
 			};
 
-			// Each asset has some configuration; current amount of supply,
-			// total amount that can be supplied and the name of the account 
-			// that controls the supply of the asset.
 			struct [[eosio::table("assetstats")]] AssetStats {
 				asset    supply;
 				asset    max_supply;
@@ -88,7 +76,7 @@ namespace SmtsBusiness {
 			// Table configuration
 			typedef multi_index<"accounts"_n, Account>               Accounts;
 			typedef multi_index<"stat"_n, AssetStats>                Stats;
-			typedef multi_index<"cunitprice"_n, CommodityUnitPrice>  CommodityUnitPrices;
+			typedef multi_index<"cunitprices"_n, CommodityUnitPrice> CommodityUnitPrices;
 			typedef multi_index<"inventreqs"_n, InventoryReq>        InventoryReqs;
 	};
 }
