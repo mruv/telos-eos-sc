cb4967dbf09e8523cd8635bc0b872cab79f69b48
Vincent Mwendwa <vrubben@outlook.com>
2019-04-17 10:32:05 +0300

    added React.js support

---
+++ web-app/webpack.config.js
@@ -0,0 +1,43 @@
+var path = require('path');
+
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './ui/index.js',
+    devtool: 'sourcemaps',
+    cache: false,
+    mode: 'development',
+    output: {
+        path: __dirname,
+        filename: './built/bundle.js'
+    }, module: {
+        rules: [{
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                use: [{
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ["@babel/preset-env", "@babel/preset-react"]
+                        }
+                    }]
+            },
+            {
+                test: /\.css$/,
+                use: [{loader: 'style-loader'}, {loader: 'css-loader'}]
+            },
+            {
+                test: /\.(ttf|eot|svg|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: [{loader: 'file-loader'}]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [{loader: 'file-loader'}]
+            },
+            {
+                test: /\.(ttf|eot|woff|woff2|svg)$/,
+                loader: "url-loader?limit=10&name=fonts/[hash].[ext]"
+            }
+        ]
+    }, plugins: [new webpack.ProvidePlugin({"React": "react"})]
+};
+
